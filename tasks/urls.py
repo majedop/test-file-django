d59d02a0bc873ff8e8bd05585099069fc829e0d7
Majedul <kazimajedul20@gmail.com>
2026-07-21 17:14:11 +0600

    ulrs bug fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from tasks.views import show_task, show_new_features
 
 urlpatterns = [
-    path("show-task/", show_task)
+    path("show-task/", show_task),
     path("show_feature/<int:id>/", show_new_features)
 ]
